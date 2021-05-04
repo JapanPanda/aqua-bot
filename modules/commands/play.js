d9434fd32b5df2582c672ddb5fa4f7b6e39d86aa
JapanPanda <japanxpanda@yahoo.com>
2021-05-03 23:55:15 -0700

    Made play command use embeds

--- modules/commands/play.js
+++ modules/commands/play.js
@@ -1,13 +1,17 @@
 const { queueAudio } = require('../audio');
+const { createAnnounce } = require('../utils');
 
 module.exports = {
   name: 'play',
   description: 'Plays a youtube video in the current voice channel.',
   async execute(message, args) {
     if (args.length === 0) {
-      message.inlineReply(
-        'Incorrect usage, $play [youtube link or search query]'
+      const embed = createAnnounce(
+        'Incorrect Usage',
+        'Incorrect usage, $play [youtube link or search query]',
+        '#ffbaba'
       );
+      message.inlineReply(embed);
       return;
     }
     queueAudio(message, args, false);
