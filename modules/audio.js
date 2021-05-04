6f778f6dce29da0e60ca8b03b63723cece268eda
JapanPanda <japanxpanda@yahoo.com>
2021-05-03 17:51:04 -0700

    Refactored audio.js to be more modular

--- modules/audio.js
+++ modules/audio.js
@@ -111,6 +111,115 @@ const playAudio = async (message, args, isPredefined) => {
   createDispatcher(audio, audioData, audioOptions, connection, guild_id);
 };
 
+const queuePlaylist = async (message, args, guildGlobal) => {
+  try {
+    const query = args.join(' ');
+    const playlistId = await ytpl.getPlaylistID(query);
+    const playlistResult = await ytpl(playlistId, { limit: Infinity });
+
+    const shouldPlay = guildGlobal.queue.length === 0;
+    let fitAll = true;
+    let queueString = playlistResult.items.reduce((acc, ele, i) => {
+      if (i === 1) {
+        acc = '';
+      }
+
+      const string = `**${guildGlobal.queue.length + i}.** [${ele.title}](${
+        ele.shortUrl
+      })\n`;
+
+      if (acc.length + string.length > 1021) {
+        fitAll = false;
+        return acc;
+      }
+
+      return acc + string;
+    });
+
+    if (playlistResult.items.length === 1) {
+      queueString = `**${guildGlobal.queue.length + i}.** [${ele.title}](${
+        ele.shortUrl
+      })\n`;
+    }
+
+    if (!fitAll) {
+      queueString += '...';
+    }
+
+    for (const ele of playlistResult.items) {
+      guildGlobal.queue.push({
+        message: message,
+        audio: [ele.shortUrl],
+        title: ele.title,
+        isPredefined: false,
+      });
+    }
+
+    const queueEmbed = new discord.MessageEmbed()
+      .setColor('#edca1a')
+      .addFields({
+        name: `Added ${playlistResult.items.length} videos to the queue`,
+        value: queueString,
+      });
+
+    message.inlineReply(queueEmbed);
+
+    if (shouldPlay) {
+      playAudio(message, args, false);
+    }
+  } catch (err) {
+    logger.error(
+      `Error while trying to parse youtube playlist link.\n${err.stack}`
+    );
+    message.inlineReply('Invalid youtube playlist link!');
+    return;
+  }
+};
+
+const queueYoutubeVideo = async (message, args, guildGlobal) => {
+  // if it is not a playlist
+  // TODO: maybe add the ability to choose which video when doing a query
+  const query = args.join(' ');
+  const srResults = await ytsr(query, { limit: 1 });
+  if (srResults.results === 0) {
+    message.inlineReply('Invalid youtube link!');
+    return;
+  }
+
+  const video = srResults.items[0];
+  title = video.title;
+  if (
+    args.length === 1 &&
+    args[0].includes('https://www.youtube.com') &&
+    video.url !== args[0]
+  ) {
+    message.inlineReply('Invalid youtube link!');
+    return;
+  } else {
+    args = [video.url];
+  }
+
+  const queueEmbed = new discord.MessageEmbed().setColor('#edca1a').addFields({
+    name: 'Added to Queue',
+    value: `[${title}](${args[0]})`,
+  });
+
+  message.inlineReply(queueEmbed);
+
+  // add to guild's queue
+  guildGlobal.queue.push({
+    message: message,
+    audio: args,
+    title: title,
+    isPredefined: false,
+  });
+
+  // if there is nothing in the queue besides itself, go ahead and play it
+  if (guildGlobal.queue.length === 1) {
+    playAudio(message, args, false);
+  }
+};
+
 const queueAudio = async (message, args, isPredefined) => {
   // user isn't in a voice channel
   if (!message.member.voice.channel) {
@@ -134,104 +243,16 @@ const queueAudio = async (message, args, isPredefined) => {
   }
 
   let title = null;
-  if (isPredefined) {
+  const query = args.join(' ');
+  // check to see if it is a playlist
+  if (ytpl.validateID(query)) {
+    queuePlaylist(message, args, guildGlobal);
+    return;
+  } else if (isPredefined) {
     title = args[0];
   } else {
-    const query = args.join(' ');
-    // check to see if it is a playlist
-    if (ytpl.validateID(query)) {
-      try {
-        const playlistId = await ytpl.getPlaylistID(query);
-        const playlistResult = await ytpl(playlistId, { limit: Infinity });
-
-        const shouldPlay = guildGlobal.queue.length === 0;
-        let fitAll = true;
-        let queueString = playlistResult.items.reduce((acc, ele, i) => {
-          if (i === 1) {
-            acc = '';
-          }
-
-          const string = `**${guildGlobal.queue.length + i}.** [${ele.title}](${
-            ele.shortUrl
-          })\n`;
-
-          if (acc.length + string.length > 1021) {
-            fitAll = false;
-            return acc;
-          }
-
-          return acc + string;
-        });
-
-        if (playlistResult.items.length === 1) {
-          queueString = `**${guildGlobal.queue.length + i}.** [${ele.title}](${
-            ele.shortUrl
-          })\n`;
-        }
-
-        if (!fitAll) {
-          queueString += '...';
-        }
-
-        for (const ele of playlistResult.items) {
-          guildGlobal.queue.push({
-            message: message,
-            audio: [ele.shortUrl],
-            title: ele.title,
-            isPredefined: false,
-          });
-        }
-
-        const queueEmbed = new discord.MessageEmbed()
-          .setColor('#edca1a')
-          .addFields({
-            name: `Added ${playlistResult.items.length} videos to the queue`,
-            value: queueString,
-          });
-
-        message.inlineReply(queueEmbed);
-
-        if (shouldPlay) {
-          playAudio(message, args, isPredefined);
-        }
-      } catch (err) {
-        logger.error(
-          `Error while trying to parse youtube playlist link.\n${err.stack}`
-        );
-        message.inlineReply('Invalid youtube playlist link!');
-        return;
-      }
-    } else {
-      // if it is not a playlist
-      // TODO: maybe add the ability to choose which video when doing a query
-      const srResults = await ytsr(query, { limit: 1 });
-      if (srResults.results === 0) {
-        message.inlineReply('Invalid youtube link!');
-        return;
-      }
-
-      const video = srResults.items[0];
-      title = video.title;
-      if (
-        args.length === 1 &&
-        args[0].includes('https://www.youtube.com') &&
-        video.url !== args[0]
-      ) {
-        message.inlineReply('Invalid youtube link!');
-        return;
-      } else {
-        args = [video.url];
-      }
-
-      const queueEmbed = new discord.MessageEmbed()
-        .setColor('#edca1a')
-        .addFields({
-          name: 'Added to Queue',
-          value: `[${title}](${args[0]})`,
-        });
-
-      message.inlineReply(queueEmbed);
-    }
+    queueYoutubeVideo(message, args, guildGlobal);
+    return;
   }
 
   // add to guild's queue
